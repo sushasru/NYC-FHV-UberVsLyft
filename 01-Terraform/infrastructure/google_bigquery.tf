410506a06a12de344debc2e607a0da884988aff4
Susha Subramaniaru <sushasru@gmail.com>
2023-04-26 21:53:36 -0400

    Update google_bigquery.tf

--- 01-Terraform/infrastructure/google_bigquery.tf
+++ 01-Terraform/infrastructure/google_bigquery.tf
@@ -1,5 +1,5 @@
 resource "google_bigquery_dataset" "nyc_fhvhv_BQuery"{
-    dataset_id =  "nyc-fhvhv_tripdetails_zones"
+    dataset_id =  "nyc_fhvhv_tripdetails_zones"
     friendly_name =  "TripDetailsAndZones"
     description = "This is the Bigquery dataset with trip details and nyc taxi zones combined"
     location = var.google_region
