93806fc3c7569c63efce22b121d1d25b2f729494
Susha Subramaniaru <sushasru@gmail.com>
2023-04-26 21:48:23 -0400

    Create google_bigquery.tf
    adding tf for creating BigQuery

---
+++ 01-Terraform/infrastructure/google_bigquery.tf
@@ -0,0 +1,10 @@
+resource "google_bigquery_dataset" "nyc_fhvhv_BQuery"{
+    dataset_id =  "nyc-fhvhv_tripdetails_zones"
+    friendly_name =  "TripDetailsAndZones"
+    description = "This is the Bigquery dataset with trip details and nyc taxi zones combined"
+    location = var.google_region
+}
+
+resource "google_bigquery_table" "TripDetailsAndZones" {
+    dataset_id =  google_bigquery_dataset.nyc_fhvhv_BQuery.dataset_id
+}
